7e8e1d7457b2f3eb7db445fea4d056288728c7d0
Aaryash <31902160+DirtyVibe@users.noreply.github.com>
2020-09-24 20:15:45 +0530

    Delete strrev.sh

--- strrev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Adding the file into .bashrc to se the Alias for quick usage.
-#alias rev='bash /home/dirtyvibe/strrev.sh'
-
-read -p "Input: " STRING
-
-if [[ -n "$STRING" ]]; then echo $STRING | rev
-else echo "Error"
-fi
